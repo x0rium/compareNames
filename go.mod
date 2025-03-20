eaea307b8dd3d0785d25f262123872686604e639
John Doe <monolyth@tequila.boom>
2025-03-20 17:17:09 +0300

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/x0rium/compareNames
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,48 @@
+package compareNames
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/x0rium/compareNames/api"
+)
+
+func main() {
+	// Парсим аргументы командной строки
+	port := flag.Int("port", 8080, "HTTP server port")
+	flag.Parse()
+
+	// Настраиваем роуты
+	router := api.SetupRoutes()
+
+	// Создаем сервер
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", *port),
+		Handler:      router,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  30 * time.Second,
+	}
+
+	// Запускаем сервер в горутине
+	go func() {
+		log.Printf("Запуск сервера на порту %d", *port)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Ошибка запуска сервера: %v", err)
+		}
+	}()
+
+	// Канал для обработки сигналов завершения работы (Ctrl+C)
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("Выключение сервера...")
+	log.Println("API сервер выключен")
+}
